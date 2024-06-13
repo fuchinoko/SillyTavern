dc98bc2b9d76e965fd8386fc74fd8cf3dabba51a
fuchinoko <flimeblock@gmail.com>
2024-06-13 14:48:15 +0300

    fix: remove console.log

--- public/scripts/RossAscends-mods.js
+++ public/scripts/RossAscends-mods.js
@@ -942,7 +942,6 @@ export function initRossMods() {
     function processHotkeys(event) {
         // alt+a create new chat
         if ((event.altKey && event.key == 'a') || (event.altKey && event.key == 'ф')) {
-            console.log('hey')
             event.preventDefault();
             $('#option_start_new_chat').trigger('click');
             $('#dialogue_popup_ok').trigger('click')
